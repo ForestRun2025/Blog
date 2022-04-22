50950db55623a3e509ee19a8a88f1ef4a706ea9e
ForestRun2025 <wilsonsuccess@gmail.com>
2022-04-22 17:08:44 -0500

    Codemy #7

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from crypt import methods
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from wtforms import StringField, SubmitField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
@@ -36,4 +35,6 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form submitted successful")
+    
     return render_template("name.html", name = name, form = form)
