5d4dc95f3daf81a90d0fab2bca9b0dfb83a0dcc5
ForestRun2025 <wilsonsuccess@gmail.com>
2022-04-22 17:08:22 -0500

    expo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from crypt import methods
 from flask import Flask, render_template
 from wtforms import StringField, SubmitField
-from flask_wtf 
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
 
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "LoveMyWife"
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What is your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -18,3 +26,14 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
+
+#Create Name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()  
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name = name, form = form)
